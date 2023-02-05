26caed9dd8db811e51e159058855b4309ff8e1bd
sajedulislamsaju <sajedulislamsaju123@gmail.com>
2023-02-05 19:24:03 +0600

    the array Loop is add

---
+++ arrayLoop.js
@@ -0,0 +1,15 @@
+// this is the arrey loop in the section .
+
+var numbers = [34, 93, 93 ,90 ,45,46,67,34,64,65,23,45,];
+
+for (i = 0 ; i< numbers.length ;i++){
+    var number = numbers[i];
+    console.log(number);
+}
+
+
+var items = ['mouse','pen','keyboard','mobile','box','laptop'];
+for (i = 0; i < items.length; i++){
+    var item = items[i];
+    console.log(item);
+}
